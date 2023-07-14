8f1a71ca3b19a25e82c82c0341da127ac1fa3cf3
Simona Miller <simona.miller@ldas-pcdev1.ligo-la.caltech.edu>
2023-07-14 05:03:36 -0500

    new scripts and figures for reweighting + ppcs

---
+++ Code/PopulationInference/posterior_predictive_draws.py
@@ -0,0 +1,234 @@
+import numpy as np 
+import pylab
+
+import json
+import random
+import sys 
+import os
+
+sys.path.append('../../Figures/')
+from fig_helper_functions import *
+
+'''
+Load data
+'''
+
+# For loading emcee samples
+model = 'betaPlusGaussian'
+pops = ['1', '2', '3']
+nevents = ['70', '330']
+date = '070523'
+
+f_root = '/home/simona.miller/measuring-bbh-component-spin/Data/PopulationInferenceOutput/'
+
+# Make dicts
+all_emcee_data = {}
+all_sampleDicts_rw = {}
+
+# Cycle through the different populations and number of events
+for pop in pops: 
+    
+    emcee_data_pop = {}
+    rw_data_pop = {}
+    
+    for nevent in nevents:
+      
+        # emcee output
+        name = f_root+f"{model}/{date}_{model}_pop{pop}_{nevent}events.json"
+        with open(name, 'r') as f:
+            pop_data = json.load(f)
+            emcee_data_pop[nevent+'events'] = pop_data 
+
+        # weighted sample dicts corresponding to the above emcee samples
+        name2 = f_root+f"{model}/{date}_{model}_{pop}_{nevent}events_reweighted_sampleDict.json"
+        with open(name2) as f: 
+            rw_event_data = json.load(f)
+            rw_data_pop[nevent+'events'] = rw_event_data 
+    
+    all_emcee_data['pop'+pop] = emcee_data_pop
+    all_sampleDicts_rw['pop'+pop] = rw_data_pop
+    
+# Load injectionDict
+with open(f_root+"../PopulationInferenceInput/injectionDict_full_mass_range.json", 'r') as f: 
+    injectionDict = json.load(f)
+
+# Unpack injections
+chi1s = np.asarray(injectionDict['a1'])
+chi2s = np.asarray(injectionDict['a2'])
+cost1s = np.asarray(injectionDict['cost1'])
+cost2s = np.asarray(injectionDict['cost2'])
+m1s = np.asarray(injectionDict['m1'])
+m2s = np.asarray(injectionDict['m2'])
+zs = np.asarray(injectionDict['z'])
+dVdzs = np.asarray(injectionDict['dVdz'])
+
+# Draw probability for component spins, masses, + redshift
+p_draw = np.asarray(injectionDict['p_draw_a1a2cost1cost2'])*np.asarray(injectionDict['p_draw_m1m2z'])
+
+# Number of items in injectonDict
+ninjections = len(chi1s)
+
+
+'''
+Generate predicted versus observed draws from populations
+'''
+
+# Number of catalogs to create
+nCatalogs = 1000
+
+# Create dict for predicted versus observed "catalogs" 
+pred_vs_obs_spins = {}
+
+# Cycle through populations
+for pop_key in ['pop1', 'pop2', 'pop3']:
+
+    # Print progress
+    print(pop_key)
+    pred_vs_obs_spins_pop = {}
+
+    for nevents in [70, 330]:
+
+        # Print progress
+        events_key = str(nevents)+'events'
+        print(events_key)
+
+        # Fetch emcee data for this population
+        emcee_data = all_emcee_data[pop_key][events_key]
+
+        # Create arrays in which to store the predicted spin parameters
+        chi1_pred = np.zeros((nCatalogs, nevents))
+        chi2_pred = np.zeros((nCatalogs, nevents))
+        cost1_pred = np.zeros((nCatalogs, nevents))
+        cost2_pred = np.zeros((nCatalogs, nevents))
+        chieff_pred = np.zeros((nCatalogs, nevents))
+        chip_pred = np.zeros((nCatalogs, nevents))
+
+        # And the observed spin parameters 
+        chi1_obs = np.zeros((nCatalogs, nevents))
+        chi2_obs = np.zeros((nCatalogs, nevents))
+        cost1_obs = np.zeros((nCatalogs, nevents))
+        cost2_obs = np.zeros((nCatalogs, nevents))
+        chieff_obs = np.zeros((nCatalogs, nevents))
+        chip_obs = np.zeros((nCatalogs, nevents))
+
+        # Generate nCatalog instantiations of "catalogs"
+        for run in np.arange(nCatalogs):
+
+            '''
+            "Predicted" spins
+            '''
+
+            # Choose numinjections hyperparameters and draw a sample from each corresponding distribution
+            nhyperparams = len(emcee_data['mu_chi']['processed'])
+            random_indices = np.random.choice(nhyperparams, size=nevents, replace=False)
+
+            for i,ind in enumerate(random_indices):
+
+                # Fetch parameters 
+                mu_chi = emcee_data['mu_chi']['processed'][ind]
+                sigma_chi = emcee_data['sigma_chi']['processed'][ind]
+                mu_cost = emcee_data['mu_cost']['processed'][ind]
+                sigma_cost = emcee_data['sigma_cost']['processed'][ind]
+                Bq = emcee_data['Bq']['processed'][ind]
+
+                # transform from mu and sigma to a and b for beta distribution
+                a, b = mu_sigma2_to_a_b(mu_chi, sigma_chi**2)
+
+                # Calculate weights on values from injectionDict
+                p_chi1 = calculate_betaDistribution(chi1s, a, b)
+                p_chi2 = calculate_betaDistribution(chi2s, a, b)
+                p_cost1 = calculate_Gaussian_1D(cost1s, mu_cost, sigma_cost, -1, 1)
+                p_cost2 = calculate_Gaussian_1D(cost2s, mu_cost, sigma_cost, -1, 1)
+                p_masses = p_astro_masses(m1s, m2s, bq=Bq)
+                p_z = p_astro_z(zs, dV_dz=dVdzs)
+
+                weights = p_chi1*p_chi2*p_cost1*p_cost2*p_masses*p_z/p_draw
+                weights_normed = weights/np.sum(weights)
+                weights_normed[np.where(weights_normed<0)] = 0 # get rid of tiny division errors
+
+                # Select a sample with probabilities given by the normalized weights
+                idx = np.random.choice(ninjections, p=weights_normed, size=1) 
+                chi1 = chi1s[idx]
+                chi2 = chi2s[idx]
+                cost1 = cost1s[idx]
+                cost2 = cost2s[idx]
+
+                # Calculate chi_eff 
+                q = m2s[idx]/m1s[idx]
+                chi_eff = calculate_chiEff(chi1, chi2, cost1, cost2, q)
+
+                # Calculate chi_p
+                sint1 = np.sin(np.arccos(cost1))
+                sint2 = np.sin(np.arccos(cost2))
+                chi_p = calculate_chiP(chi1, chi2, sint1, sint2, q)
+
+                # Add to arrays
+                chi1_pred[run,i] = chi1
+                chi2_pred[run,i] = chi2
+                cost1_pred[run,i] = cost1
+                cost2_pred[run,i] = cost2
+                chieff_pred[run,i] = chi_eff
+                chip_pred[run,i] = chi_p
+
+
+            '''
+            Observed spins
+            '''
+
+            # Load reweighted individual event samples
+            sampleDict_rw = all_sampleDicts_rw[pop_key][events_key]
+
+            # Cycle through events
+            for j, name in enumerate(sampleDict_rw.keys()):
+
+                d = sampleDict_rw[name]['reweighted']
+
+                # Select a random sample
+                idx = random.choice(np.arange(len(d['chi1'])))
+                chi1 = d['chi1'][idx]
+                chi2 = d['chi2'][idx]
+                cost1 = d['cost1'][idx]
+                cost2 = d['cost2'][idx]
+
+                # Calculate chi_eff
+                q = d['m2'][idx]/d['m1'][idx]
+                chi_eff = calculate_chiEff(chi1, chi2, cost1, cost2, q)
+
+                # Calculate chi_p
+                sint1 = np.sin(np.arccos(cost1))
+                sint2 = np.sin(np.arccos(cost2))
+                chi_p = calculate_chiP(chi1, chi2, sint1, sint2, q)
+
+                # Add to arrays
+                chi1_obs[run,j] = chi1
+                chi2_obs[run,j] = chi2
+                cost1_obs[run,j] = cost1
+                cost2_obs[run,j] = cost2
+                chieff_obs[run,j] = chi_eff
+                chip_obs[run,j] = chi_p
+
+        # Add results to dict 
+        pred_vs_obs_spins_pop[events_key] = {
+            'predicted':{
+                'chi1':chi1_pred.tolist(),
+                'chi2':chi2_pred.tolist(),
+                'cost1':cost1_pred.tolist(),
+                'cost2':cost2_pred.tolist(),
+                'chieff':chieff_pred.tolist(), 
+                'chip':chip_pred.tolist()
+            },
+            'observed':{
+                'chi1':chi1_obs.tolist(),
+                'chi2':chi2_obs.tolist(),
+                'cost1':cost1_obs.tolist(),
+                'cost2':cost2_obs.tolist(),
+                'chieff':chieff_obs.tolist(), 
+                'chip':chip_obs.tolist()
+            }
+        }
+
+    pred_vs_obs_spins[pop_key] = pred_vs_obs_spins_pop
+
+# Save
+with open(f_root+'pred_vs_obs_spins_dict_pp.json', "w") as f:
+    json.dump(pred_vs_obs_spins, f)
---
+++ Code/PopulationInference/reweighting.py
@@ -0,0 +1,211 @@
+import numpy as np
+import sys
+from scipy.special import erf
+from scipy.special import beta
+import json 
+
+import astropy.cosmology as cosmo
+import astropy.units as u
+from astropy.cosmology import Planck15
+
+from posterior_helper_functions import * 
+
+"""
+Function to do posterior reweighting 
+
+    sampleDict = dictionary containing individual event samples
+    hyperPEDict = dictionary containing hyperparemeter samples: from json file that emcee outputs
+    
+"""
+
+def pop_reweight(sampleDict, hyperPEDict): 
+    
+    # Number of hyperparameter samples
+    nHyperPESamps = len(hyperPEDict['mu_chi']['processed'])
+    
+    # dict in which to put reweighted individual event samples
+    sampleDict_rw = {}
+    
+    # cycle through events
+    for k, event in enumerate(sampleDict): 
+        
+        print(f"reweighting event {k+1} of {len(sampleDict)}: {event}", end='\r')
+        
+        # Unpack posterior samples for this event
+        chi1_samples = np.asarray(sampleDict[event]['a1'])
+        chi2_samples =  np.asarray(sampleDict[event]['a2'])
+        cost1_samples = np.asarray(sampleDict[event]['cost1'])
+        cost2_samples = np.asarray(sampleDict[event]['cost2'])
+        m1_samples = np.asarray(sampleDict[event]['m1'])
+        m2_samples = np.asarray(sampleDict[event]['m2'])
+        z_samples = np.asarray(sampleDict[event]['z'])
+        dVdz_samples = np.asarray(sampleDict[event]['dVc_dz'])
+
+        # indices corresponding to each sample for these events (will be used below in the for loop)
+        nSamples = len(chi1_samples)
+        indices = np.arange(nSamples)
+        
+        # arrays in which to store reweighted samples for this event
+        new_chi1_samps = np.zeros(nHyperPESamps)
+        new_chi2_samps = np.zeros(nHyperPESamps)
+        new_cost1_samps = np.zeros(nHyperPESamps)
+        new_cost2_samps = np.zeros(nHyperPESamps)
+        new_mass1_samps = np.zeros(nHyperPESamps)
+        new_mass2_samps = np.zeros(nHyperPESamps)
+        
+        # cycle through hyper PE samps
+        for i in range(nHyperPESamps):
+            
+            '''iterating through each curve of the trace plot'''
+            
+            if model == 'betaPlusGaussian':
+                
+                # Fetch i^th hyper PE sample: 
+                mu_chi = hyperPEDict['mu_chi']['processed'][i]
+                sigma_chi = hyperPEDict['sigma_chi']['processed'][i]
+                mu_cost = hyperPEDict['mu_cost']['processed'][i]
+                sigma_cost = hyperPEDict['sigma_cost']['processed'][i]
+                Bq = hyperPEDict['Bq']['processed'][i]
+
+                # Translate mu_chi and sigma_chi to beta function parameters a and b 
+                # See: https://en.wikipedia.org/wiki/Beta_distribution#Mean_and_variance
+                a, b = mu_sigma2_to_a_b(mu_chi, sigma_chi**2.)
+
+                # Evaluate model at the locations of samples for this event
+                p_chi1 = calculate_betaDistribution(chi1_samples, a, b)
+                p_chi2 = calculate_betaDistribution(chi2_samples, a, b)
+                p_cost1 = calculate_Gaussian_1D(cost1_samples, mu_cost, sigma_cost, -1, 1)
+                p_cost2 = calculate_Gaussian_1D(cost2_samples, mu_cost, sigma_cost, -1, 1)
+                
+            elif model == 'betaPlusDoubleGaussian':
+                
+                # Fetch i^th hyper PE sample: 
+                mu_chi = hyperPEDict['mu_chi']['processed'][i]
+                sigma_chi = hyperPEDict['sigma_chi']['processed'][i]
+                mu1_cost = hyperPEDict['mu1_cost']['processed'][i]
+                sigma1_cost = hyperPEDict['sigma1_cost']['processed'][i]
+                mu2_cost = hyperPEDict['mu2_cost']['processed'][i]
+                sigma2_cost = hyperPEDict['sigma2_cost']['processed'][i]
+                MF_cost = hyperPEDict['MF_cost']['processed'][i]
+                Bq = hyperPEDict['Bq']['processed'][i]
+
+                # Translate mu_chi and sigma_chi to beta function parameters a and b 
+                # See: https://en.wikipedia.org/wiki/Beta_distribution#Mean_and_variance
+                a, b = mu_sigma2_to_a_b(mu_chi, sigma_chi**2.)
+
+                # Evaluate model at the locations of samples for this event
+                p_chi1 = calculate_betaDistribution(chi1_samples, a, b)
+                p_chi2 = calculate_betaDistribution(chi2_samples, a, b)
+                p_cost1 = calculate_Double_Gaussian(cost1_samples, mu1_cost, sigma1_cost, mu2_cost, sigma2_cost, MF_cost, -1, 1)
+                p_cost2 = calculate_Double_Gaussian(cost2_samples, mu1_cost, sigma1_cost, mu2_cost, sigma2_cost, MF_cost, -1, 1)
+            
+        
+            # Pop dist for all four params combined is a product of each four individual dists
+            pSpins = p_chi1*p_chi2*p_cost1*p_cost2
+            
+            # PE priors for chi_i and cost_i are all uniform, so we set them to unity here
+            nSamples = pSpins.size
+            spin_PE_prior = np.ones(nSamples)
+            
+            # Need to reweight by astrophysical priors on m1, m2, z ...
+            # - p(m1)*p(m2)
+            p_astro_m1_m2 = p_astro_masses(m1_samples, m2_samples, bq=Bq)
+            old_m1_m2_prior = np.power(1.+z_samples, 2) # PE prior on masses is uniform in DETECTOR FRAME component masses
+            # - p(z)
+            p_astro_redshift = p_astro_z(z_samples, dV_dz=dVdz_samples)
+            old_z_prior = p_astro_z(z_samples, dV_dz=dVdz_samples, kappa=0) # see: bilby.gw.prior.UniformSourceFrame prior 
+            # - For full m1, m2, z prior reweighting: 
+            m1_m2_z_prior_ratio = (p_astro_m1_m2/old_m1_m2_prior)*(p_astro_redshift/old_z_prior) 
+            
+            # calculate weights for this hyper parameter
+            weights = pSpins*m1_m2_z_prior_ratio/spin_PE_prior
+            weights = weights/np.sum(weights)
+            
+            # select a random sample from the event posterior subject to these weights
+            j = np.random.choice(indices, p=weights)
+            
+            # populate the new sample arrays with this random sample
+            new_chi1_samps[i] = chi1_samples[j]
+            new_chi2_samps[i] = chi2_samples[j]
+            new_cost1_samps[i] = cost1_samples[j]
+            new_cost2_samps[i] = cost2_samples[j]
+            new_mass1_samps[i] = m1_samples[j]
+            new_mass2_samps[i] = m2_samples[j]
+        
+        # Add into reweighted sampleDict
+        downsample_idxs = np.random.choice(indices, size=nHyperPESamps)
+        sampleDict_rw[event] = {
+            'original':{
+                'chi1':np.asarray(sampleDict[event]['a1'])[downsample_idxs].tolist(),
+                'chi2':np.asarray(sampleDict[event]['a2'])[downsample_idxs].tolist(),
+                'cost1':np.asarray(sampleDict[event]['cost1'])[downsample_idxs].tolist(), 
+                'cost2':np.asarray(sampleDict[event]['cost2'])[downsample_idxs].tolist(),
+                'm1':np.asarray(sampleDict[event]['m1'])[downsample_idxs].tolist(),
+                'm2':np.asarray(sampleDict[event]['m2'])[downsample_idxs].tolist()
+            },
+            'reweighted':{
+                'chi1':new_chi1_samps.tolist(),
+                'chi2':new_chi2_samps.tolist(),
+                'cost1':new_cost1_samps.tolist(),
+                'cost2':new_cost2_samps.tolist(),
+                'm1':new_mass1_samps.tolist(),
+                'm2':new_mass2_samps.tolist()
+            }
+        }
+
+    return sampleDict_rw
+
+
+"""
+Actually loading and running pop reweighting
+"""
+
+if __name__=="__main__":
+    
+    # To translate between pop # and full name used in saving
+    pop_names_dict = {
+        '1':'population1_highSpinPrecessing', 
+        '2':'population2_mediumSpin', 
+        '3':'population3_lowSpinAligned'
+    }
+    
+    # Directory home 
+    froot = '../../Data/'
+    
+    # Run settings we want to reweight
+    dates = ['070523']
+    models = ['betaPlusGaussian', 'betaPlusDoubleGaussian']
+    pops = ['1', '2', '3']
+    nevents = ['330']
+        
+    # Cycle through them all 
+    for date in dates: 
+        for model in models:
+            for pop in pops: 
+                for nevent in nevents: 
+                    
+                    print(f'{date}, {model}, pop {pop}, {nevent} events')
+    
+                    # Cycle through runs we want to reweight
+
+                    # Load dict with individual event PE samples (Load sampleDict):
+                    with open(froot+f'PopulationInferenceInput/sampleDict_{pop_names_dict[pop]}_full_mass_range.json') as f:
+                        sampleDict_full = json.load(f) 
+
+                    # Load population parameter PE samps
+                    with open(froot+f'PopulationInferenceOutput/{model}/{date}_{model}_pop{pop}_{nevent}events.json', 'r') as f:
+                        hyperPEDict = json.load(f)
+
+                    # Select only events from sampleDict used in this emcee run
+                    events = hyperPEDict['events_used']
+                    sampleDict = {event:sampleDict_full[event] for event in events}
+
+                    # Run reweighting for default model
+                    sampleDict_rw = pop_reweight(sampleDict, hyperPEDict)   
+
+                    # Save results
+                    savename = froot+f'PopulationInferenceOutput/{model}/{date}_{model}_{pop}_{nevent}events_reweighted_sampleDict.json'
+                    with open(savename, "w") as f:        
+                        json.dump(sampleDict_rw,f)
+                        
+                    print('\n') 
